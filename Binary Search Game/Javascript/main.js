bd007e547ce26df2f63897b4134c413295c0e9cd
Jeriah Neal <26jneal@redmondschools.org>
2024-03-04 11:44:16 -0800

    Initial Commit

---
+++ Binary Search Game/Javascript/main.js
@@ -0,0 +1,69 @@
+// Binary Search Game
+
+const startButton = $("#start");
+const higherButton = $("#higher");
+const lowerButton = $("#lower");
+const correctButton = $("#correct");
+const message = $("#message");
+const guess = $("#guess");
+const minRange = 0;
+const maxRange = 100;
+let currentGuess = 50;
+let guessCount = 0;
+let guessCorrect = false;
+let minGuess = 0;
+let maxGuess = 100;
+
+function addEventListeners() {
+  startButton.click(Start);
+  higherButton.click(Higher);
+  lowerButton.click(Lower);
+  correctButton.click(Correct);
+}
+
+function Higher() {
+  guessCount++;
+  minGuess = currentGuess;
+  currentGuess = Math.floor((maxGuess + minGuess) / 2);
+  message.text("Is your number " + currentGuess + "?");
+}
+
+function Lower() {
+  guessCount++;
+  maxGuess = currentGuess;
+  currentGuess = Math.floor((maxGuess + minGuess) / 2);
+  message.text("Is your number " + currentGuess + "?");
+}
+
+function Correct() {
+  guessCorrect = true;
+  message.text("I got it in " + guessCount + " guesses!");
+}
+
+function Start() {
+  guessCount = 0;
+  guessCorrect = false;
+  minGuess = 0;
+  maxGuess = 100;
+  currentGuess = 50;
+  message.text("Is your number " + currentGuess + "?");
+  startButton.hide();
+  higherButton.show();
+  lowerButton.show();
+  correctButton.show();
+  guess.show();
+}
+
+function init() {
+  addEventListeners();
+  startButton.show();
+  higherButton.hide();
+  lowerButton.hide();
+  correctButton.hide();
+  guess.hide();
+  message.text("Press Start to begin");
+}
+
+$(document).ready(function () {
+  init();
+});
